75afb1401c2393aed921e9284b2cae8feca03edc
dollydasheep <MelPoPo21@gmail.com>
2025-08-07 18:21:35 +0800

    motion react for animations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
+import { AnimatePresence, motion } from "motion/react"
 
 const Home = () => {
 
   const [pdfFile, setPdfFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [submitComplete, setSubmitComplete] = useState(false);
 
   const handleFileInput = (e) => {
     setPdfFile(e.target.files[0])
@@ -15,6 +18,8 @@ const Home = () => {
 
     const formData = new FormData();
     formData.append('pdfFile', pdfFile);
+
+    setSubmitComplete(true);
 
     try{
       const res = await fetch("http://localhost:3000/prompt?questionCount=10",{
@@ -33,19 +38,32 @@ const Home = () => {
 
   return (
     <div className='w-full h-screen flex justify-center items-center p-4 box-border'>
-      <div className='w-lg rounded-lg bg-teal-800 p-4 box-border flex flex-col items-center'>
-        <h1 className='text-white font-bold text-2xl mb-2 place-self-start'>AI Quiz Creator</h1>
-        <p className='text-gray-200/70 text-sm mb-4 place-self-start'>makes studying easier with personalized quizzes for faster, smarter reviews</p>
-        <div className='w-full border-b border-gray-100/80 mb-4'></div>
-        <form onSubmit={handleSubmit} className='place-self-start mb-4'>
-          <div>
-            <label htmlFor="" className='text-white'>Upload Study Material <span className='text-gray-200/70 text-xs'>(pdf,ppt,etc.)</span></label>
-            <Input onChange={handleFileInput} type="file" className='my-2' />
-          </div>
-          <Button type="submit" value="Submit" className="w-30 py-2">Submit</Button>
-        </form>
-        
-      </div>
+      {isLoading && (
+        <motion.h1 initial={{ opacity: 0, scale: 0.5}} animate={{ opacity: 1, scale: 1 }}>Hello</motion.h1>
+      )}
+      <AnimatePresence onExitComplete={()=>{setIsLoading(true)}}>
+        {!submitComplete && (
+          <motion.div 
+          transition={{
+            type: "tween",
+            duration: 0.3,
+            ease: "easeInOut"
+          }}
+          exit={{ opacity: 0, scale: 0.5, rotate: 45 }}
+          className='w-lg rounded-lg bg-teal-800 p-4 box-border flex flex-col items-center'
+          >
+            <h1 className='text-white font-bold text-2xl mb-2 place-self-start'>AI Quiz Creator</h1>
+            <p className='text-gray-200/70 text-sm mb-4 place-self-start'>makes studying easier with personalized quizzes for faster, smarter reviews</p>
+            <div className='w-full border-b border-gray-100/80 mb-4'></div>
+            <form onSubmit={handleSubmit} className='place-self-start mb-4'>
+              <div>
+                <label htmlFor="" className='text-white'>Upload Study Material <span className='text-gray-200/70 text-xs'>(pdf,ppt,etc.)</span></label>
+                <Input onChange={handleFileInput} type="file" className='my-2' />
+              </div>
+              <Button type="submit" value="Submit" className="w-30 py-2">Submit</Button>
+            </form>
+          </motion.div >)}
+      </AnimatePresence>
     </div>
   )
 }
