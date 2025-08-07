3865b07c4d418c0b8aca5d398be03155f461ccf8
dollydasheep <MelPoPo21@gmail.com>
2025-08-07 17:58:50 +0800

    api integration

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,6 +6,7 @@ const Input = ({ className = "", ...props }) => {
       <input 
         {...props}
         className={`file:text-white file:text-base file:border-0 file:inline-flex file:font-medium text-white text-xs
+                    box-border
                     ring ring-gray-200 rounded-lg px-3 py-1 outline-none
                     bg-teal-700
                     focus:border focus:border-white focus-visible:border-red-500 focus-visible:ring-[3px] focus-visible:ring-red-500/50 focus:outline-none
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 const Home = () => {
+
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const handleFileInput = (e) => {
+    setPdfFile(e.target.files[0])
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('pdfFile', pdfFile);
+
+    try{
+      const res = await fetch("http://localhost:3000/prompt?questionCount=10",{
+        method: 'POST',
+        encType: 'multipart/form-data',
+        body: formData
+      })
+      const data = await res.json();
+      
+      console.log(data)
+
+    } catch(err){
+      console.error(err);
+    }
+  }
+
   return (
     <div className='w-full h-screen flex justify-center items-center p-4 box-border'>
       <div className='w-lg rounded-lg bg-teal-800 p-4 box-border flex flex-col items-center'>
         <h1 className='text-white font-bold text-2xl mb-2 place-self-start'>AI Quiz Creator</h1>
         <p className='text-gray-200/70 text-sm mb-4 place-self-start'>makes studying easier with personalized quizzes for faster, smarter reviews</p>
         <div className='w-full border-b border-gray-100/80 mb-4'></div>
-        <form action="" className='place-self-start mb-4'>
+        <form onSubmit={handleSubmit} className='place-self-start mb-4'>
           <div>
             <label htmlFor="" className='text-white'>Upload Study Material <span className='text-gray-200/70 text-xs'>(pdf,ppt,etc.)</span></label>
-            <Input type="file" className='my-2' />
+            <Input onChange={handleFileInput} type="file" className='my-2' />
           </div>
+          <Button type="submit" value="Submit" className="w-30 py-2">Submit</Button>
         </form>
-        <Button className="w-30 py-2">Submit</Button>
+        
       </div>
     </div>
   )
