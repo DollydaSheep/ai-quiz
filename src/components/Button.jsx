2336276b0f1134529184ac1a1bd3dbdb54ff009b
dollydasheep <MelPoPo21@gmail.com>
2025-08-12 10:20:51 +0800

    result page redone

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge';
 
 const Button = ({className, children , ...props}) => {
   return (
-    <button {...props} className={`bg-cyan-500 py-1 px-3 hover:bg-cyan-500/80 rounded-lg cursor-pointer outline-none focus-visible:ring-green-800/80 focus-visible:ring-[2px]
-                                    text-white ${className}`}>
+    <button {...props} className={twMerge(
+                                  `bg-cyan-500 py-2 px-3 hover:bg-cyan-500/80 rounded-lg cursor-pointer outline-none focus-visible:ring-green-800/80 focus-visible:ring-[2px]
+                                  text-white transition duration-200 `,
+                                  className
+                                  )}
+    >
       {children}
     </button>
   )
--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Button from '../../components/Button'
+import { TrophyIcon } from 'lucide-react'
 
 const Result = () => {
   return (
     <div className='w-full h-screen flex justify-center items-center p-4 box-border'>
-      <div className='w-lg flex flex-col items-center p-4 bg-teal-800 rounded-lg'>
-        <h1 className='text-white text-8xl font-bold my-4'>10 / 10</h1>
-        <p className='text-gray-300 mb-4 text-lg '>You did a great job!</p>
-        <div className='w-full rounded-lg bg-gray-200 mb-4'>
-          <div className={`p-1 w-full bg-green-500 rounded-lg place-self-start transition-all duration-500 ease-in-out`}></div>
+      <div className='w-md flex flex-col items-center p-4 bg-teal-800 rounded-lg'>
+        <TrophyIcon className='text-yellow-500 size-15 mt-4' />
+        <h1 className='text-white text-8xl font-medium mb-6'>10/10</h1>
+        <p className='text-gray-200 mb-6 text-lg '>You did a great job!</p>
+        <div className='w-full rounded-lg bg-gray-200 mb-8'>
+          <div className={`p-0.75 w-full bg-green-500 rounded-lg place-self-start transition-all duration-500 ease-in-out`}></div>
         </div>
-        <div className='w-full grid grid-cols-2 gap-2 mb-2'>
-          <Button>Retry</Button>
-          <Button>Save</Button>
+        <div className='w-full grid grid-cols-2 gap-4 mb-4'>
+          <Button className="bg-white hover:bg-white/20 text-green-600 hover:text-white">Retry</Button>
+          <Button className="bg-white hover:bg-white/20 text-green-600 hover:text-white">Save</Button>
         </div>
         <Button className="w-full">Back</Button>
       </div>
